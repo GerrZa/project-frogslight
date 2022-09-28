6b940a35a7af31504447b47dd0ab909238bf7136
GerrZa <thanapatr.ger@gmail.com>
2022-09-29 00:01:25 +0700

    all mechanic done-polished

--- scr/obj/warp_hole.gd
+++ scr/obj/warp_hole.gd
@@ -1,7 +1,16 @@
+tool
 extends Area2D
 
 export var target_hole = NodePath()
 onready var target_pos = get_node(target_hole).global_position
+
+var idle_hole_frame = [0,15,30]
+
+export(int,0,2) var hole_type
+
+func _process(delta):
+	if $Sprite/AnimationPlayer.is_playing() == false:
+		$Sprite.frame = idle_hole_frame[hole_type]
 
 func get_target_pos():
 	
@@ -13,4 +22,14 @@ func jump_play_anim():
 	get_node(target_hole).play_anim()
 
 func play_anim():
-	$Sprite/AnimationPlayer.play("activate")
+	$Sprite/AnimationPlayer.play("open_hole" + String(hole_type))
+	
+	yield($Sprite/AnimationPlayer,"animation_finished")
+	
+	$CollisionShape2D.disabled = true
+
+
+func _on_warp_hole_area_exited(area):
+	if area is Froggy:
+		
+		$CollisionShape2D.disabled = false
--- scr/tool/singletons/global.gd
+++ scr/tool/singletons/global.gd
@@ -1,3 +1,23 @@
 extends Node
 
 var current_level = 0
+
+var level_unlocked = [
+	true,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false,
+	false
+]
+
+var default_level_unlocked
+
+var setting = {"fullscreen" : false}
