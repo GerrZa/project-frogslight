e1413a7d0fe195b480fdf317821134356c515842
GerrZa <thanapatr.ger@gmail.com>
2022-10-23 23:54:11 +0700

    credit scene

--- scr/level/mainmenu/quit.gd
+++ scr/level/mainmenu/quit.gd
@@ -1,4 +1,8 @@
 extends ButtonClass
 
 func on_button_clicked():
+	$AnimationPlayer.play("selected")
+	
+	yield($AnimationPlayer,"animation_finished")
+	
 	get_tree().quit()
---
+++ scr/ui/credit_name/credit_scene.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+var stopping_thanks = false
+var is_stopped = false
+
+var menu_scene = preload("res://scr/level/mainmenu/menu.tscn")
+
+func _process(delta):
+	
+	if stopping_thanks == false:
+		$text_holder.position.y -= 50 * delta
+	
+	if $text_holder.position.y <= -122 and is_stopped == false:
+		stopping_thanks = true
+		
+		yield(get_tree().create_timer(2),"timeout")
+		
+		stopping_thanks = false
+		is_stopped = true
+		
+func _input(event):
+	if Input.is_action_just_pressed("ui_esc"):
+		get_tree().change_scene_to(menu_scene)
+
+func _on_VisibilityNotifier2D_screen_entered():
+	get_tree().change_scene_to(menu_scene)
