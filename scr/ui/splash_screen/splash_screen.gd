5d0155f483bd915394e997378041b47aa2e41ace
GerrZa <thanapatr.ger@gmail.com>
2023-10-08 21:39:51 +0700

    lil update :P

--- scr/ui/splash_screen/splash_screen.gd
+++ scr/ui/splash_screen/splash_screen.gd
@@ -8,34 +8,35 @@ var short_whoosh_sfx = preload("res://asset/fx/sound_fx/menu_sfx/sfx_shortwhoosh
 signal start
 
 func _ready():
-	$audio_controller.play("start")
+	$"%audio_controller".play("start")
 	
 	$trizg_logo.visible = false
 	
 	yield(get_tree().create_timer(0.3),"timeout")
 	
 	$trizg_logo.visible = true
-	$AnimationPlayer.play("riseup_trizg")
+	$"%AnimationPlayer".play("riseup_trizg")
 	
-	yield($AnimationPlayer,"animation_finished")
+	yield($"%AnimationPlayer","animation_finished")
 	
 	$trizg_logo.frame = 0
 	$trizg_logo.play("default")
 	
 	yield($trizg_logo,"animation_finished")
 	
-	$AnimationPlayer.play("default")
+	print('hefldwsahf')
+	$"%AnimationPlayer".play("default")
 	
-	yield($AnimationPlayer,"animation_finished")
+	yield($"%AnimationPlayer","animation_finished")
 	
 	$trizg_logo/godot_logo.frame = 0
 	$trizg_logo/godot_logo.play("default")
 	
 	yield($trizg_logo/godot_logo,"animation_finished")
 	
-	$AnimationPlayer.play("wipe_godot_out")
+	$"%AnimationPlayer".play("wipe_godot_out")
 	
-	yield($AnimationPlayer,"animation_finished")
+	yield($"%AnimationPlayer","animation_finished")
 	
 	get_tree().change_scene_to(menu)
 
